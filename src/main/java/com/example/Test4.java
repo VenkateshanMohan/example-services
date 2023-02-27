ab6bea75a3a72f947fc234f5b917c15cffec79ac
Venkateshan Mohan <mvenkateshan@gmail.com>
2023-02-26 23:16:40 -0500

    test

--- src/main/java/com/example/Test4.java
+++ src/main/java/com/example/Test4.java
@@ -5,11 +5,8 @@ import org.apache.commons.lang3.tuple.Triple;
 
 public class Test4 {
     public static void main(String... a) {
-        triples();
-    }
-
-    private static <T, U, V> void triples() {
         Triple<String, String, String> triple = new MutableTriple<>();
         System.out.println(triple);
     }
+
 }
